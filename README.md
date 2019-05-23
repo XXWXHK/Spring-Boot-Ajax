dfa9d516a4812640ed2f250f6b01d1437e3e13e8
徐晓伟 <xuxiaowei@live.hk>
2019-05-23 17:19:57 +0800

    新建项目提交。

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Spring-Boot-Ajax
 Spring Boot Ajax 跨域
+
+# 依赖
+
+## Spring Boot 依赖（创建项目时选择）
+
+- Spring Boot
+    - 2.1.5.RELEASE
+    
+- Web
+    - Web                       使用Tomcat和Spring MVC进行全栈Web开发
+    
+- Template
+	- Thymeleaf                 Thymeleaf模板引擎（页面）
+
+    
---
+++ src/main/java/cn/com/xuxiaowei/ajax/AjaxApplication.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei.ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ */
+@SpringBootApplication
+public class AjaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxApplication.class, args);
+    }
+
+}
